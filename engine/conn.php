17a64594ca31d5a0380ebbaffac91a378cdbea4b
Imo Douglas <imodouglas@gmail.com>
2021-02-05 20:54:09 +0000

    Delete conn.php

--- engine/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conn = new PDO("mysql:host=localhost;dbname=eoffice",'root','');
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  	$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
-
-	session_start();
-
-error_reporting(E_ALL);
-?>
